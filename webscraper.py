e19d5c7ada37b25e7b25fca7622c2233ae278ca4
Michael Huang <jmichael.huang@gmail.com>
2018-06-25 17:31:19 -0700

    Add files via upload

---
+++ webscraper.py
@@ -0,0 +1,63 @@
+import urllib2
+from bs4 import BeautifulSoup
+import csv
+
+def create_html(link):
+	page = urllib2.urlopen(link)
+	html = BeautifulSoup(page, 'html.parser')
+	return html
+
+def get_title(link):
+	html = create_html(link)
+	unref_title = html.find('h1', attrs = {'id': 'artTitle'})
+	title = unref_title.text.strip() 		
+	return title
+
+def get_DOI(link):
+	html = create_html(link)
+	unref_DOI = html.find('li', attrs = {'id': 'artDoi'}) 
+	DOI = unref_DOI.text.strip() 
+	ref_DOI = DOI[16:]  							#cut out url part
+	return ref_DOI
+
+def get_authors(link):
+	num_authors = 0 
+	final_author = '' 
+	author_present = True
+	html = create_html(link)
+
+	while(author_present):
+		unref_author = html.find('a', attrs = {'data-author-id': str(num_authors),'class': 'author-name'})
+		if unref_author is None:  					#if no further author found
+			author_present = False
+			break
+		author = unref_author.text.strip()
+		ref_author = ''
+		for ch in author:
+			if ch.isalpha() or ch.isspace(): 		#get rid of non-alpha chars, leave space between first/last name
+				ref_author += ch
+		if(num_authors != 0)
+			final_author += ', ' + ref_author
+		num_authors = num_authors + 1
+	return final_author
+
+def setup_data(link):
+	data_row = []
+
+	data_row.append(get_title(link))
+	data_row.append(get_DOI(link))
+	data_row.append(get_authors(link))
+
+	return data_row
+
+def print_article_info(links): 						#input array of links
+	headers = ['Paper Title', 'DOI', 'Author(s)']
+	data = []
+	for link in links:
+		data.append(setup_data(link))
+
+	with open('data.csv', 'a') as csv_file:
+		writer = csv.writer(csv_file)
+		writer.writerow(headers)  					#headers at top of excel file
+		for row in data:
+			writer.writerow(row)  					#add data to excel file
